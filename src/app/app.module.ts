dd5a78729688dce097830ff0a117f5f4704f4579
Evgeny_MKH <trisemerka@gmail.com>
2025-03-18 19:27:16 +0500

    Удаление файла app.routing и его зависимости

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -16,7 +15,6 @@ import {NgForOf} from "@angular/common";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     NgForOf
   ],
